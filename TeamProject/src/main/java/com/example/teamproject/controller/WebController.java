cae39010bb14ed83f0c1aa3abbff43cc0ce17f89
RiGun-K <1234suho@naver.com>
2021-11-12 17:51:43 +0900

    2021/11/12

--- TeamProject/src/main/java/com/example/teamproject/controller/WebController.java
+++ TeamProject/src/main/java/com/example/teamproject/controller/WebController.java
@@ -1,10 +1,27 @@
 package com.example.teamproject.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.teamproject.repository.MenuRepository;
+import com.example.teamproject.repository.UserRepository;
+import com.example.teamproject.data.Menu;
+import com.example.teamproject.mapper.MenuMapper;
 
 @Controller
 public class WebController {
+	
+
+	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired 
+	MenuRepository menuRepository;
 
 	
 	@GetMapping("/denied")
@@ -43,5 +60,11 @@ public class WebController {
 		return "AdminMain";
 	}
 	
+	///////////////////////////////// 관리자 /////////////////////
 	
+	@GetMapping("/pcheck")
+	public String pcheck(Model model) {
+		model.addAttribute("menus", menuRepository.findAll());
+		return "Pcheck";
+	}
 }
