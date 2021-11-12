bccaa7b38fe563a33dedb212cdb01157238f6bb3
RiGun-K <1234suho@naver.com>
2021-11-13 02:49:56 +0900

    2021/11/13

--- TeamProject/src/main/java/com/example/teamproject/controller/WebController.java
+++ TeamProject/src/main/java/com/example/teamproject/controller/WebController.java
@@ -23,27 +23,25 @@ public class WebController {
 	@Autowired 
 	MenuRepository menuRepository;
 
-	
-	@GetMapping("/denied")
-	public String denied() {
-		return "denied";
-	}
-	
-	@GetMapping("/adduser")
-	public String user() {
-		return "addUser";
-	}
-	
-
+	//  FIRST PAGE
 	@GetMapping("/")
 	public String index() {
 		return "index";
 	}
 	
+	// CART PAGE
 	@GetMapping("/cart")
 	public String cart() {
 		return "Cart";
 	}
+
+	// PAYMENT PAGE
+	@GetMapping("/payment")
+	public String payment() {
+		return "Payment";
+	}
+	
+	///////////////////////////////// 로그인 /////////////////////
 	
 	@GetMapping("/login")
 	public String login() {
@@ -55,22 +53,38 @@ public class WebController {
 		return "AdminRegister";
 	}
 	
+	
+	
+	///////////////////////////////// 관리자 /////////////////////
+	
+	// MAIN PAGE ( 로그인 이후 )
 	@GetMapping("/main")
 	public String main() {
 		return "AdminMain";
 	}
 	
-	///////////////////////////////// 관리자 /////////////////////
-	
+	// PRODUCT REGISTER
 	@GetMapping("/pregister")
 	public String pregister() {
 		return "Pregister";
 	}
 	
-	
+	// PRODUCT CHECK
 	@GetMapping("/pcheck")
 	public String pcheck(Model model) {
 		model.addAttribute("menus", menuRepository.findAll());
 		return "Pcheck";
+	}
+	
+	// PRODUCT MODIFY
+	@GetMapping("/pmodify")
+	public String pmodify() {
+		return "Pmodify";
+	}
+	
+	// PRODUCT SALES STATS
+	@GetMapping("/pstats")
+	public String pstats() {
+		return "Pstats";
 	}
 }
