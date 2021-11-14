3bce11bd4dfd6e98eeaebf146bbc3aade491973c
RiGun-K <1234suho@naver.com>
2021-11-14 17:57:02 +0900

    2021/11/14

--- TeamProject/src/main/java/com/example/teamproject/data/Menu.java
+++ TeamProject/src/main/java/com/example/teamproject/data/Menu.java
@@ -1,10 +1,13 @@
 package com.example.teamproject.data;
 
 import java.sql.Blob;
+import java.time.LocalDateTime;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
@@ -20,12 +23,16 @@ import javax.persistence.Table;
 public class Menu {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	// 우선 자동으로 등록되도록 설정했음  
 	private int menuid;
+	
 	@Column(nullable=false)
 	private String menuname;
 	private int price;
 	private Blob image;
 	private String ex;
+	private LocalDateTime savedTime;
 	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "kind")
@@ -34,6 +41,12 @@ public class Menu {
 	// 생성자 우선 생략
 	// kindid(기본키) 와 맵핑해야됨. 
 	
+	public LocalDateTime getSavedTime() {
+		return savedTime;
+	}
+	public void setSavedTime(LocalDateTime savedTime) {
+		this.savedTime = savedTime;
+	}
 	public Menu(){}
 	public Menu(int menuid, String menuname, int price, Blob image, String ex) {
 		this.menuid = menuid;
