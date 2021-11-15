b6d4ecf0ac9d67d2556fff7d6e58f342661e61ae
RiGun-K <1234suho@naver.com>
2021-11-15 15:52:59 +0900

    11/15

--- TeamProject/src/main/java/com/example/teamproject/repository/MenuRepository.java
+++ TeamProject/src/main/java/com/example/teamproject/repository/MenuRepository.java
@@ -3,9 +3,11 @@ package com.example.teamproject.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.teamproject.data.Menu;
 
+@Repository
 public interface MenuRepository extends JpaRepository<Menu, Integer> {
 
 
